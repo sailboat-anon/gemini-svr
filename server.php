96b6a1060e3600cd434f85e693da132dd78d13d5
sba <sailboatanon@protonmail.com>
2021-02-13 06:52:24 -0800

    works!

--- server.php
+++ server.php
@@ -1,18 +1,18 @@
 <?php
-$config = array([
+$config = array(
 	'logging'		=>	true,
 	'log_file' 	=>	'logs/server.log',
 	'log_sep' 	=>	'\t',
-	'cert_file'	=> 	'certs/sailboat-anon.space',
+	'cert_file'	=> 	getcwd().'/certs/sailboat-anon.space/combined.pem',
 	'cert_pass' 	=> 	'password',
 	'local_ip' 	=> 	'127.0.0.1',
 	'local_port'	=> 	'1965',
-	'hosted_sites_dir' 		=> 'hosts/',
-	'default_dir'				=>	'hosts/gemini-web-svcs/',
+	'hosted_sites_dir' 		=> getcwd().'/hosts/',
+	'default_dir'				=>	getcwd().'/hosts/sailboat-anon.space/',
 	'acceptable_index_files'	=>	array('index.gemini', 'index.gmi'),
 
-]);
-
+);
+print_r($config);
 if(empty($config['cert_file'])) die("> Missing cert {$config['cert_file']} \n");
 if(!is_readable($config['cert_file']))die("> Cert is unreadable: {$config['cert_file']} \n");
 
@@ -22,6 +22,7 @@ stream_context_set_option($context, 'ssl', 'local_cert', $config['cert_file']);
 stream_context_set_option($context, 'ssl', 'passphrase', $config['cert_pass']);
 stream_context_set_option($context, 'ssl', 'allow_self_signed', true);
 stream_context_set_option($context, 'ssl', 'verify_peer', false);
+stream_context_set_option($context, 'ssl', 'cafile', $config['cert_file']);
 
 $socket = stream_socket_server("tcp://{$config['local_ip']}:{$config['local_port']}", $errno, $errstr, STREAM_SERVER_BIND|STREAM_SERVER_LISTEN, $context);
 stream_socket_enable_crypto($socket, false);
@@ -39,8 +40,9 @@ while(true) {
 	stream_set_blocking($forkedSocket, false);
 
 	$parsed_url = parse_request($line);
+	
 	$filepath = get_filepath($parsed_url);
-
+	print_r($filepath);
 	$status_code = get_status_code($filepath);
 
 	$meta = "";
@@ -51,12 +53,12 @@ while(true) {
 		$content = file_get_contents($filepath);	
 		$filesize = filesize($filepath);
 	} else {
-		$meta = "Not found";
+		$meta = "Not found: {$status_code}";
 	}
 
 	$status_line = $status_code." ".$meta;
-	if($g->logging)
-		$g->log_to_file($remoteIP,$status_code, $meta, $filepath, $filesize);
+	//if($g->logging)
+		//$g->log_to_file($remoteIP,$status_code, $meta, $filepath, $filesize);
 	$status_line .= "\r\n";
 	fwrite($forkedSocket, $status_line);
 
@@ -100,7 +102,7 @@ function get_filepath($url) {
 	if(!empty($url['host'])) $hostname = $url['host'];
 	$valid_hosts = get_valid_hosts();
 	if(!in_array($hostname, $valid_hosts))
-		$hostname = "default";
+		$hostname = "localhost";
 
 	$url['path'] = str_replace(array("..", "__"), "", $url['path']);
 	// force an index file to be appended if a filename is missing
@@ -109,10 +111,10 @@ function get_filepath($url) {
 	} elseif(substr($url['path'], -1) == "/") {
    $url['path'] .= $config['acceptable_index_files'][0]; // extend later
 }
-$valid_data_dir = dirname(__FILE__)."/".($config['hosted_sites_dir']);
+
 $return_path = ($config['hosted_sites_dir']).$hostname.$url['path'];
 // check the real path is in the data_dir (path traversal sanity check)
-if(substr(realpath($return_path),0, strlen($valid_data_dir)) == $valid_data_dir) {
+if(substr(realpath($return_path),0, strlen($config['hosted_sites_dir'])) == $config['hosted_sites_dir']) {
    return $return_path;
    }
    return false;
