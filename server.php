6fad2c02f501b409ff6e9f462e372803d16b6be3
sba <sailboatanon@protonmail.com>
2021-02-13 14:26:27 -0800

    stable v0

--- server.php
+++ server.php
@@ -3,19 +3,19 @@
 $config = array(
 	'logging'		=>	true,
 	'log_file' 	=>	getcwd().'/logs/server.log',
-	'log_sep' 	=>	',',
+	'log_sep' 	=>	'|',
 	'cert_file'	=> 	getcwd().'/certs/sailboat-anon.space/combined.pem',
 	'cert_pass' 	=> 	'password',
 	'local_ip' 	=> 	'127.0.0.1',
 	'local_port'	=> 	'1965',
 	'hosted_sites_dir' 		=> getcwd().'/hosts/',
 	'default_dir'				=>	getcwd().'/hosts/sailboat-anon.space/',
-	'acceptable_index_files'	=>	array('index.gemini', 'index.gmi')
+	'acceptable_index_files'	=>	array('index.gmi', 'index.gemini')
 );
 
 if(empty($config['cert_file'])) die("> Missing cert {$config['cert_file']} \n");
 if(!is_readable($config['cert_file']))die("> Cert is unreadable: {$config['cert_file']} \n");
-file_put_contents($config['log_file'], json_encode($config), FILE_APPEND);
+//file_put_contents($config['log_file'], json_encode($config), FILE_APPEND); // debug mode only!
 
 $context = stream_context_create();
 stream_context_set_option($context, 'ssl', 'local_cert', $config['cert_file']);
@@ -25,28 +25,27 @@ stream_context_set_option($context, 'ssl', 'verify_peer', false);
 stream_context_set_option($context, 'ssl', 'cafile', $config['cert_file']);
 
 $socket = stream_socket_server("tcp://{$config['local_ip']}:{$config['local_port']}", $errno, $errstr, STREAM_SERVER_BIND|STREAM_SERVER_LISTEN, $context);
-stream_socket_enable_crypto($socket, false);
-// only allow 1.2+
-$cryptoMethod = STREAM_CRYPTO_METHOD_TLS_SERVER
-	& ~ STREAM_CRYPTO_METHOD_TLSv1_0_SERVER
-	& ~ STREAM_CRYPTO_METHOD_TLSv1_1_SERVER;
 
 while (true) {
-	$forkedSocket = stream_socket_accept($socket, "-1", $remoteIP);
-	stream_set_blocking($forkedSocket, true);
-	stream_socket_enable_crypto($forkedSocket, true, $cryptoMethod);
-	$line = fread($forkedSocket, 1024);
-	stream_set_blocking($forkedSocket, false);
+	$hsocket = stream_socket_accept($socket, '-1', $remote_ip); // -1 is 'daemon'
+	stream_set_blocking($hsocket, true);
+	stream_socket_enable_crypto($hsocket, true, STREAM_CRYPTO_METHOD_TLSv1_2_SERVER); // enforce TLS 1.2
+	// downgrade
+	/*$cryptoMethod = STREAM_CRYPTO_METHOD_TLS_SERVER
+		& ~ STREAM_CRYPTO_METHOD_TLSv1_0_SERVER
+		& ~ STREAM_CRYPTO_METHOD_TLSv1_1_SERVER;
+	*/
+	$line = fread($hsocket, 1024);
+	stream_set_blocking($hsocket, false);
 
 	$parsed_url = parse_request($line);
-	
 	$filepath = get_filepath($parsed_url);
 	$status_code = get_status_code($filepath);
 
-	$meta = "";
+	$meta = '';
 	$filesize = 0;
 	
-	if($status_code == "20") {
+	if($status_code == 20) {
 		$meta = get_mime_type($filepath);
 		$content = file_get_contents($filepath);	
 		$filesize = filesize($filepath);
@@ -56,16 +55,15 @@ while (true) {
 
 	$status_line = $status_code." ".$meta;
 	if ($config['logging']) {
-		log_to_file($remoteIP, $status_code, $meta, $filepath, $filesize);
+		log_to_file($remote_ip, $status_code, $meta, $filepath, $filesize);
 	}
 	$status_line .= "\r\n";
-	fwrite($forkedSocket, $status_line);
+	fwrite($hsocket, $status_line);
 
-	if($status_code == "20") {
-		fwrite($forkedSocket,$content);
+	if($status_code == 20) {
+		fwrite($hsocket,$content);
 	}
-
-	fclose($forkedSocket);
+	fclose($hsocket);
 }
 
 function parse_request($request) {
@@ -79,22 +77,20 @@ function get_valid_hosts() {
 	return $dirs;
 }
 
-function get_status_code($filepath) {
-	if(is_file($filepath) and file_exists($filepath)) return '20';
-	if(!file_exists($filepath)) return '51';
-	return '50';
+function get_status_code($filepath): string {
+	if(is_file($filepath) and file_exists($filepath)) return 20;
+	if(!file_exists($filepath)) return 51;
+	return 50;
 }
 
-function get_mime_type($filepath) {
+function get_mime_type($filepath): string {
 	$type = mime_content_type($filepath);
-	// detect gemini file type
-	// so.. if it ends with gemini (or if it has no extension), assume
 	$path_parts = pathinfo($filepath);
-	if(empty($path_parts['extension']) or $path_parts['extension'] == "gemini") $type = "text/gemini"; // add .gmi
+	if(empty($path_parts['extension']) or ($path_parts['extension'] == 'gemini') || ($path_parts['extension'] == 'gmi')) $type = 'text/gemini';
 	return $type;
 }
 
-function get_filepath($url) {
+function get_filepath($url): string {
 	global $config;
 	$hostname = "";
 	if(!is_array($url))	return false;
@@ -102,27 +98,23 @@ function get_filepath($url) {
 	$valid_hosts = get_valid_hosts();
 	if(!in_array($hostname, $valid_hosts))
 		$hostname = "localhost";
-
-	$url['path'] = str_replace(array("..", "__"), "", $url['path']);
-	// force an index file to be appended if a filename is missing
-	if(empty($url['path'])) {
-		$url['path'] = "/".($config['acceptable_index_files'][0]);
-	} elseif(substr($url['path'], -1) == "/") {
-   $url['path'] .= $config['acceptable_index_files'][0]; // extend later
-}
-
-$return_path = ($config['hosted_sites_dir']).$hostname.$url['path'];
-// check the real path is in the data_dir (path traversal sanity check)
-if(substr(realpath($return_path),0, strlen($config['hosted_sites_dir'])) == $config['hosted_sites_dir']) {
-   return $return_path;
-   }
-   return false;
+	
+	$url['path'] = str_replace(array("/..", "__"), "", $url['path']); 
+	if(substr($url['path'], -1) == "/") {
+		if (is_readable($config['acceptable_index_files'][0])) {
+			$extension = '.gmi';
+		}
+		$url['path'] .= $config['acceptable_index_files'][1]; // allow .gemini, prefer .gmi
+	}
+	$return_path = ($config['hosted_sites_dir']).$hostname.$url['path'];
+	// path traversal sanity check
+	if(substr(realpath($return_path), 0, strlen($config['hosted_sites_dir'])) == $config['hosted_sites_dir']) { return $return_path; }
 }
 
 function log_to_file($ip, $status_code, $meta, $filepath, $filesize) {
 	global $config;
-	$ts = date("Y-m-d H:i:s", strtotime('now'));
-	$str = "\n".$ts.$config['log_sep'].$ip.$config['log_sep'].$status_code.$config['log_sep'].$meta.$config['log_sep'].$filepath.$config['log_sep'].$filesize;
+	$ts = date('Y-m-d H:i:s', strtotime('now'));
+	$str = $ts.$config['log_sep'].$ip.$config['log_sep'].$status_code.$config['log_sep'].$meta.$config['log_sep'].$filepath.$config['log_sep'].$filesize."\n";
 	print_r($str);
 	file_put_contents($config['log_file'], $str, FILE_APPEND);
 }
